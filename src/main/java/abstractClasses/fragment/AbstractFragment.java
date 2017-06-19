8aebb7057e7aa9091222a5d39bf98ae1be93594c
Anton Naidonov <Anton_Naidonov@epam.com>
2017-06-19 16:12:04 +0300

    Extended field decorator added (to decorate Abstract Fragments)

--- src/main/java/abstractClasses/fragment/AbstractFragment.java
+++ src/main/java/abstractClasses/fragment/AbstractFragment.java
@@ -1,11 +1,92 @@
 package abstractClasses.fragment;
 
 import helpers.Browser;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.PageFactory;
 
-public abstract class AbstractFragment {
+import java.util.List;
+
+public abstract class AbstractFragment implements WebElement {
+    @Override
+    public void click() {
+
+    }
+
+    @Override
+    public void submit() {
+
+    }
+
+    @Override
+    public void sendKeys(CharSequence... keysToSend) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String getTagName() {
+        return null;
+    }
+
+    @Override
+    public String getAttribute(String name) {
+        return null;
+    }
+
+    @Override
+    public boolean isSelected() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public String getText() {
+        return null;
+    }
+
+    @Override
+    public List<WebElement> findElements(By by) {
+        return null;
+    }
+
+    @Override
+    public WebElement findElement(By by) {
+        return null;
+    }
+
+    @Override
+    public Point getLocation() {
+        return null;
+    }
+
+    @Override
+    public Dimension getSize() {
+        return null;
+    }
+
+    @Override
+    public Rectangle getRect() {
+        return null;
+    }
+
+    @Override
+    public String getCssValue(String propertyName) {
+        return null;
+    }
+
+    @Override
+    public <X> X getScreenshotAs(OutputType<X> target) throws WebDriverException {
+        return null;
+    }
+
     protected WebDriver browser;
 
     @SuppressWarnings("unchecked")
