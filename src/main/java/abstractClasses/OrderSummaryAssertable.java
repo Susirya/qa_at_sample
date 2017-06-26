e7f7a20b6b74d9c70d3a56140cf01ddbf4d188f7
Anton Naidonov <anton_naidonov@epam.com>
2017-06-26 18:24:16 +0300

    Added default implementation in abstractClasses.OrderSummaryAssertable

--- src/main/java/abstractClasses/OrderSummaryAssertable.java
+++ src/main/java/abstractClasses/OrderSummaryAssertable.java
@@ -3,5 +3,8 @@ package abstractClasses;
 public interface OrderSummaryAssertable {
     boolean isOrderSubtotalEqual(String givenSubtotal);
     boolean isOrderDeliveryEqual(String givenSubtotal);
+    default boolean isOrderTaxesEqual(String givenSubtotal){
+        return false;
+    }
     boolean isOrderTotalEqual(String givenSubtotal);
 }
